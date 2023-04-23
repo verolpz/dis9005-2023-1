5c19b8f95df62679ee5d9287464153b68ff17c0a
verolpz <128399630+verolpz@users.noreply.github.com>
2023-04-23 17:08:16 -0400

    Nuevo cambio script

--- solemne-01/verolpz/script.js
+++ solemne-01/verolpz/script.js
@@ -1,1 +1,10 @@
 console.log("Hola! bienvenido");
+
+//Códigos extraídos del trabajo de una compañera, se añadió fecha y ancho de ventana//
+console.log("ancho: " + window.innerWidth);
+const d = new Date("2023-04-23");
+document.getElementById("fecha").innerHTML=d;
+function myFunction() {
+    document.getElementById("fecha").style.fontSize = "25px"; 
+
+  }
