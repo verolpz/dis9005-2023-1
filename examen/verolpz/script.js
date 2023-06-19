a0380b3b48c38f0df2a36b2887dd5297f96ebc58
verolpz <128399630+verolpz@users.noreply.github.com>
2023-06-19 12:43:06 -0400

    Update script.js

--- examen/verolpz/script.js
+++ examen/verolpz/script.js
@@ -1,2 +1,12 @@
+// mensaje bienvenida + fecha
+console.log("Bienvenido usuario! hoy es: ", Date(Date.now()));
 
 
+// ancho de la pagina
+console.log("ancho: " + window.innerWidth);
+
+
+// consola cuenta hasta 10
+for(i=0; i<10; i++){
+    console.count()
+  }
